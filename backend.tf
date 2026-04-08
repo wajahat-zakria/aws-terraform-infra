e075ab75d802e69e8abe5312f1b2f5eee58e87ef
zakria <wajahat.zakria46@gmail.com>
2026-04-08 23:25:41 +0530

    new2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-amed-2026-001"
+    bucket         = "my-demo-terraform-bucket-12345-1020304506"
     key            = "global/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
